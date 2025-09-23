d2eb6c75b6e6796af97b06aa56b72b177cacc6e8
Mahipal-Singh01 <mahivis0123@gmail.com>
2025-09-23 23:04:06 +0530

    New all server packages install and in frontend responsive part done

--- front-end/src/pages/Doctors.jsx
+++ front-end/src/pages/Doctors.jsx
@@ -8,6 +8,7 @@ const Doctors = () => {
   const { doctors } = useContext(AppContext);
   console.log(doctors);
   const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter, setShowFilter] = useState(false);
   const navigate = useNavigate();
 
   const applyFilter = () => {
@@ -25,7 +26,15 @@ const Doctors = () => {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+        <button
+          onClick={() => setShowFilter((prev) => !prev)}
+          className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${
+            showFilter ? "bg-primary text-white" : ""
+          }`}
+        >
+          Filters
+        </button>
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
           <p
             onClick={() =>
               speciality === "General physician"
